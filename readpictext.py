938f586b22987664e48f496957e18d0dc6bdce0d
hansendm <nesnah.daniel@gmail.com>
2022-01-15 07:24:19 -0500

    Create readpictext.py

---
+++ readpictext.py
@@ -0,0 +1,10 @@
+# pip install pytesseract
+#Note: You have to download tesseract.exe from Github
+import pytesseract
+from PIL import Image
+ 
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+ 
+t=Image.open("img.png")
+text = pytesseract.image_to_string(t, config='')
+print(text)
